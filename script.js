2d74bb08660743059efe2a9125f2ca2a7af9da4e
JaehyoSong <133918883+JaehyoSong@users.noreply.github.com>
2025-03-01 14:28:33 -0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+function showTab(tabId) {
+    let tabs = document.querySelectorAll('.tab');
+    tabs.forEach(tab => tab.classList.remove('active'));
+
+    document.getElementById(tabId).classList.add('active');
+}
